639981cd0914a1899333056369b91b1180a365e4
Abhishek <abhishek.thakur926@gmail.com>
2022-05-12 18:48:58 +0530

    on click show the label to change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState(null);
+  const [changeval, setChange] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // alert(`The name you entered was: ${name}`);
+  }
+  const myvalueshow =(event) => {
+    setChange(false)
+   // event.preventDefault();
+  alert(`The name you entered was: ${changeval}`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+<div className="tasks-container">
+        <div className="container">
+        <h1>First Task Here</h1>
+          <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">
+       {
+      changeval ?
+      <h2>{data}</h2>
+      :null}
+        </label>
+
+          </div>
+          <div className="mb-3">
+            <input type="text" name="mytext"  onChange={(t) => setData(t.target.value)} />
+          </div>
+          <div className="mb-3">
+            <button type="button" onClick={()=> setChange(true)}  >Click here</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="tasks-container">
+        <div className="container">
+          <h1>Second Task Here</h1>
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">
+              <h2>{
+              name
+            }</h2></label>
+
+          </div>
+          <div className="mb-3">
+            <input type="email" className="form-control" id="exampleFormControlInput1" 
+              onChange={(e) => setName(e.target.value)} />
+          </div>
+         </div>
+      </div>
+
+    </>
   );
 }
 
