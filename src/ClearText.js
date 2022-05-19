8c557ca2e6c46a52a89d86f707a91ab71bf28699
Abhishek <abhishek.thakur926@gmail.com>
2022-05-19 18:42:10 +0530

    Task : Remove extra white spaces from the text

--- src/ClearText.js
+++ src/ClearText.js
@@ -3,8 +3,12 @@ import { useState } from 'react'
 
 function ClearText() {
     const [dataval, setVales] = useState();
-
+    const [remove, setRemove] = useState();
     
+    const Removespace = () =>  {
+     let removespace = remove.replace(/ /g,'');
+         setRemove(removespace);
+    };
   return (
     <>
           <div className="tasks-container">
@@ -14,6 +18,27 @@ function ClearText() {
                   <div className="mb-3">
                       <input type="text" className="form-control"
                           onChange={(e) => setVales(e.target.value.length)} />
+                  </div>
+                
+                 <div className="space-footer"></div>
+              </div>
+          </div>
+
+
+          <div className="tasks-container">
+              <div className="container">
+                  <h1>Remove White Space  </h1>
+                  <div className="mb-3">    <label>{remove}</label></div>
+                  <div className="mb-3">
+                      <input type="text" className="form-control"
+                          onChange={(e) => setRemove(e.target.value)} />
+                  </div>
+                  <div className="mb-3">
+                      <button type="button"
+                          title="Convert Text To Lower Case"
+                          onClick={Removespace}
+                          color="#606070"
+                      >Click here</button>
                   </div>
                  <div className="space-footer"></div>
               </div>
